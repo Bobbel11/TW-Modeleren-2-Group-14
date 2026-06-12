c470a29ec257a6c68333745e6df2df2c97362a6c
CouchPotayto <78306894+Bobbel11@users.noreply.github.com>
2026-06-12 14:34:02 +0200

    Update surgery durations and file path in Generator.py

--- Generator.py
+++ Generator.py
@@ -1,8 +1,11 @@
+
 import pulp as lp
 import random
 import pandas as pd
+import numpy as np
 
-filepath = "/Data assignment operating room 2 Large with keys.xlsx" #overal filepath neerzetten
+
+filepath = "/content/Data assignment operating room 2 Large with keys.2.0.xlsx" #overal filepath neerzetten
 excel_df1 = pd.read_excel(filepath, sheet_name = 0, header = None)
 excel_df2 = pd.read_excel(filepath, sheet_name = 1)
 excel_df3 = pd.read_excel(filepath, sheet_name = 2)
@@ -11,13 +14,13 @@ surgeries = excel_df2["Surgery"]
 
 # Dictionary = operaties met kans en minuten
 surgery_types = {
-    0: {"kans": 0.214, "duur": (180, 240)}, #hart en vaat
-    1: {"kans": 0.099, "duur": (60, 120)}, #heup en knie
-    2: {"kans": 0.092, "duur": (45, 90)}, #Laparoscopische buikingrepen
-    3: {"kans": 0.105, "duur": (60, 120)}, #overige buikchirurgie
-    4: {"kans": 0.076, "duur": (30, 45)}, #KNO
-    5: {"kans": 0.090, "duur": (30, 45)}, #keizersnee
-    6: {"kans": 0.324, "duur": (15, 20)}, #staar
+    0: {"kans": 0.214, "duur": (283.25, 140.52)}, #hart en vaat
+    1: {"kans": 0.099, "duur": (228.18, 110.30)}, #heup en knie
+    2: {"kans": 0.092, "duur": (116.90, 78.90)}, #Laparoscopische buikingrepen
+    3: {"kans": 0.105, "duur": (218.01, 113.73)}, #overige buikchirurgie
+    4: {"kans": 0.076, "duur": (194.88, 98.54)}, #KNO
+    5: {"kans": 0.090, "duur": (166.51, 103.46)}, #keizersnee & spataderen
+    6: {"kans": 0.324, "duur": (184.88, 100.25)}, #staar & meniscus
 }
 
 # Operaties en kansen opslaan
@@ -30,8 +33,11 @@ duur_lijst = [] # Renamed to avoid conflict with the integer 'duur' variable ins
 
 for i in surgeries:
   operatie_type = random.choices(types, weights=kansen)[0]
-  min_tijd, max_tijd = surgery_types[operatie_type]["duur"]
-  current_duur = random.randint(min_tijd, max_tijd) # Store generated duration in a temporary variable
+  mean, stdev = surgery_types[operatie_type]["duur"]
+  # current_duur = random.randint(min_tijd, max_tijd)
+  current_duur = min(max(np.random.normal(),-2), 2)*stdev+mean
+  while current_duur < 0:
+    current_duur =min(max(np.random.normal(),-2), 2)*stdev+mean
 
   namen.append(operatie_type)
   duur_lijst.append(current_duur) # Append the current_duur to the list
